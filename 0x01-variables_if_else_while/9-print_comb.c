6f5e92fb8a78d9533a5e67ee6fe84cce284bd40d
wanjirumel <melissawapaul@gmail.com>
2023-02-17 13:57:13 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,12 @@ int main(void)
 
 	for (num = '0'; num <= '9'; num++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
+		putchar(d);
+		if (num != '9')
+	{
 		putchar(',');
 		putchar(' ');
+	}
 	}
 
 	putchar('\n');
