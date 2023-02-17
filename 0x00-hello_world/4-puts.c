67c48cf9bc24d87ff8e045ec1b982fb6a9dc0e64
wanjirumel <melissawapaul@gmail.com>
2023-02-16 16:15:01 -0800

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that prints puts function
+ * main -A program that prints puts function
  * Return 0 (Success)
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
