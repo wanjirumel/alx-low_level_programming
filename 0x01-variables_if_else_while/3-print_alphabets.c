9168e2a9b55970fa58c7cf3dd13c1b29932135ad
wanjirumel <melissawapaul@gmail.com>
2023-02-17 13:25:03 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,13 +12,13 @@ int main(void)
 {
 	char low;
 
-	for (low = "a"; low <= "z"; low++
+	for (low = 'a'; low <= 'z'; low++)
 	putchar(low);
 
-	for (low = "A"; LOW <= "Z"; low++)
+	for (low = 'A'; low <= 'Z'; low++)
 	putchar(low);
+
 	putchar('\n');
 
 	return (0);
-
 }
