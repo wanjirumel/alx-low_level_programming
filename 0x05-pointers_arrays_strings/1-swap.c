8eb67bf760a88db4bbf3e83f71e3e5ea68761b41
root <root@DESKTOP-1T5JS0D.localdomain>
2023-02-28 18:53:53 +0300

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,8 +3,8 @@
 
 /**
  * swap_int - swap the value of two integers
- * @*a: int to swap
- * @*b: int to swap
+ * @a: character to swap
+ * @b: character to swap
  * Return: void
  */
 void swap_int(int *a, int *b)
