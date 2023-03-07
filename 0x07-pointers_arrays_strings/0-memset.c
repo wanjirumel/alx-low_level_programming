e3851d235d331bbb69e5949563bfb7f8c9e83b3d
wanjirumel <melissawapaul@gmail.com>
2023-03-07 01:45:39 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_memset - function that fills memory with a constant byte
+ * @s: charactr
+ * @b: character
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	s[i] = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character to print
+ * Return: on success 1
+ * on error -1 and error is set appropriatley
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
