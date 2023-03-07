c6e1b977f023e99a3d0b3441b8e5b647e29352a5
wanjirumel <melissawapaul@gmail.com>
2023-03-07 11:32:54 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,13 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 	if (s[i] == c)
 	{
-	return (&s[i]);
+	return (s + i);
 	}
 	}
 
-	return (NULL);
+	return ('\0');
 }
